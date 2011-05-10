8c194406cd11eb5848c8a532dedd0f211334a97e
Adolfo Builes <builes.adolfo@googlemail.com>
2011-05-10 18:04:37 -0500

    Sending file with unison

--- lib/dllt/unison.rb
+++ lib/dllt/unison.rb
@@ -1,17 +1,20 @@
-require 'posix/spawn'
-
 module DLLT
   class Unison
     def self.start(port)
       puts "starting unison server in port #{port}"
       unless @pid
-        @pid = spawn("unison -socket #{port}")
+        @pid = POSIX::Spawn::spawn("unison -socket #{port}")
       end
       puts "started unison server(pid #{@pid})"
     end
 
     def self.pid
       @pid
+    end
+
+    def self.sync_file(file, host)
+      command = "unison #{file} socket://#{host}/dictionary/dictionary.txt -auto -batch -force #{file}"
+      deferrable = EM::DeferrableChildProcess.open(command)
     end
   end
 end
