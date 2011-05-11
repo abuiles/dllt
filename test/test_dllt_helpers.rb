306c7206ea4eabcd6ccefc1ab4a08b4f0805cadd
Adolfo Builes <builes.adolfo@googlemail.com>
2011-05-10 23:52:45 -0500

    Adding test for helpers

---
+++ test/test_dllt_helpers.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'mocha'
+
+require 'dllt'
+
+describe DLLT::Helpers do
+  before do
+    DLLT::Helpers.extend DLLT::Helpers
+  end
+
+  describe "#check_reactor" do
+    it "should raise a RunTimeError if EM is not running" do
+      EM.expects(:reactor_running?).returns(false)
+
+      proc { DLLT::Helpers.check_reactor }.must_raise RuntimeError
+    end
+  end
+end
