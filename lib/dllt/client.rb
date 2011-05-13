b94b216bc1b68a0ad9bc931ec6dc9b64fe99efed
Adolfo Builes <builes.adolfo@googlemail.com>
2011-05-12 20:07:00 -0500

    Saving changes here

--- lib/dllt/client.rb
+++ lib/dllt/client.rb
@@ -21,33 +21,6 @@ module DLLT
       DRb.stop_service
     end
   end
-
-  module ClientServer
-    def self.get_ip
-      #taken from http://stackoverflow.com/questions/42566/getting-the-hostname-or-ip-in-ruby-on-rails/42923#42923
-      require 'socket'
-
-      orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
-      UDPSocket.open do |s|
-        s.connect '64.233.187.99', 1
-        s.addr.last
-      end
-    ensure
-      Socket.do_not_reverse_lookup = orig
-    end
-
-    def self.start(server_host, client_port)
-      client_url = "druby://localhost:#{client_port}"
-      server_url = "druby://#{server_host}"
-      DRb.start_service(client_url)
-      @client = DLLT::Client.new(server_url, get_ip, client_port)
-    end
-
-    def self.unregister
-      puts "\nQuitting client\n"
-      @client.unregister
-    end
-  end
 end
 
 
